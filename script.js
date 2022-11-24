eedce37d734e58c6264f365f31ab206a38c35f67
James <eugerald@email.com>
2022-11-23 20:29:48 -0600

    PAssword length validation

--- script.js
+++ script.js
@@ -10,5 +10,19 @@ function writePassword() {
 
 }
 
+
+function generatePassword(){
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  var uppercase = "IDOWHATISREQUIRED"
+  var numbers = "0123456789"
+  var symbols = "!@#$%^&*()_+~"
+
+  var passwordLength = parseInt(prompt("Please enter a valid length(8-126)?"))
+
+  if(passwordLength<8 || passwordLength>126 || isNaN(passwordLength)){
+    return "Inavlid Length entered.... Please enter a valid length"
+  }
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
